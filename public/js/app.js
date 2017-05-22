d4b7d7d77c6151436cf6b7419d136b65fae5e982
Vernon <me@example.com>
2017-05-22 10:28:45 -0500

    added ui-router, footer, stateprovider

---
+++ public/js/app.js
@@ -0,0 +1,34 @@
+angular.module('app', ['ui.router'])
+.config(function($urlRouterProvider, $stateProvider) {
+  $stateProvider
+  .state('home', {
+    templateUrl: './views/home.html',
+    url: '/',
+    controller: 'mainCtrl'
+  })
+  .state('about', {
+    templateUrl: './views/about.html',
+    url: '/about'
+  })
+  .state('join', {
+    templateUrl: './views/join.html',
+    url: '/join'
+  })
+  .state('spirit', {
+    templateUrl: './views/spirit.html',
+    url: '/spirit'
+  })
+  .state('login', {
+    templateUrl: './views/login.html',
+    url: '/login'
+  })
+  .state('contact', {
+    templateUrl: './views/contact.html',
+    url: '/contact'
+  })
+  .state('schedule', {
+    templateUrl: './views/schedule.html',
+    url: '/schedule'
+  })
+  $urlRouterProvider.otherwise('/')
+})
---
+++ public/js/mainSrv.js
@@ -0,0 +1,3 @@
+angular.module('app').service('mainSrv', function($http){
+  console.log('serving');
+})
