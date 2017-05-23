460dc7ceb03b583fe8b5241f834d58cc402cb164
Vernon <me@example.com>
2017-05-23 03:33:57 -0500

    init side-nav functionality to navbar

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -1,3 +1,10 @@
 angular.module('app').controller('mainCtrl', function($scope, mainSrv) {
   console.log('controlling');
+  $('.button-collapse').sideNav({
+     menuWidth: 300, // Default is 300
+     edge: 'left', // Choose the horizontal origin
+     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+     draggable: true // Choose whether you can drag to open on touch screens
+   }
+ );
 })
