6070c3c792db9b10832e59dcd4ec7f0629adc4b0
Vernon <vernonmu@gmail.com>
2017-05-25 15:59:37 -0500

    updating calendar api

--- index.js
+++ index.js
@@ -7,10 +7,8 @@ const fs = require('fs')
 const readline = require('readline')
 const googleAuth = require('google-auth-library')
 const axios = require('axios')
+const moment = require('moment')
 require('dotenv').config()
-// const google = require('googleapis');
-// const OAuth2 = google.auth.OAuth2;
-// var plus = google.plus('v1');
 
 const db = require('./db.js')
 
@@ -21,39 +19,13 @@ app.use(cors())
 
 app.use(express.static(__dirname + '/public'))
 
-
-// var oauth2Client = new OAuth2(
-//   process.env.CLIENT_ID,
-//   process.env.CLIENT_SECRET,
-//   process.env.REDIRECT_URL
-// );
-
-// set auth as a global default
-// google.options({
-//   auth: oauth2Client
-// });
-
-// generate a url that asks permissions for Google+ and Google Calendar scopes
-// var scopes = [
-//   'https://www.googleapis.com/auth/plus.me',
-//   'https://www.googleapis.com/auth/calendar'
-// ];
-
-// var url = oauth2Client.generateAuthUrl({
-  // 'online' (default) or 'offline' (gets refresh_token)
-  // access_type: 'offline',
-
-  // If you only need one scope you can pass it as a string
-  // scope: scopes,
-
-  // Optional property that passes state parameters to redirect URI
-  // state: { foo: 'bar' }
-// });
-
 app.get('/ptcd/cal', (req,res,next) => {
   axios.get(`https://www.googleapis.com/calendar/v3/calendars/${process.env.calendarid}/events?key=${process.env.mykey}`)
   .then((response) => {
-    // console.log(response.data);
+    // console.log(response.data.items.forEach(val => {
+      console.log(response.data);
+      // val.start.date = moment(val.start.dateTime).format("dddd, MMMM Do, h:mm")
+    }));
     res.status(200).json(response.data)
   })
   .catch( error => {
